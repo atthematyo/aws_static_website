167f0f01a73857ad8bf69a7e61159f8a73eb701e
atthematyo <johnlove1996@gmail.com>
2024-06-17 11:57:49 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,16 @@ resource "aws_s3_bucket" "main" {
 
 }
 
+resource "aws_s3_bucket_ownership_controls" "main" {
+  bucket = aws_s3_bucket.main.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "main_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.main]
+
   bucket = aws_s3_bucket.main.id
   acl    = "private"
 }
